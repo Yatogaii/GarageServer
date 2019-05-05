bf5b5a2ea77e5281f122ba88743c5615a7e5c8a8
Yatogami <781023956@qq.com>
2019-05-05 18:23:01 +0800

    first version

---
+++ Main.py
@@ -0,0 +1,72 @@
+﻿# -*- coding:utf-8 -*-
+
+import socket as Socket
+import json 
+import mysql.connector
+#用来判断动作的switcher
+ACTION_LOGIN = 100
+ACTION_GET_CAR = -100
+#用来检查账号查询结果的switcher
+RESULT_ERROR = 11
+RESULT_REGISTER = 12
+RESULT_SUCCESS = 10
+def Main():
+    soc = Socket.socket(Socket.AF_INET,Socket.SOCK_STREAM)
+    soc.bind(('0.0.0.0',8080))
+    soc.listen(20)
+    while True:
+        s , addr = soc.accept()
+        print('connect %s:%s ' % (addr))
+        # print(s.recv(1024).decode('utf-8'))
+        handleMessage(s,addr)
+
+#json解析
+# jsonStr = '{"account":"123213","password":"213"}'
+# parJson = json.loads(jsonStr)
+# print(parJson['account'])
+# print(parJson)
+
+def handleMessage(soc,addr):
+    print('start handle message')
+    recStr = soc.recv(1024).decode('utf-8')
+    jsonParser = json.loads(recStr)
+    #用字典模拟的switch
+    result = messageSwitcher[jsonParser['action']](jsonParser['account'],jsonParser['password'])    #模拟switch来处理事务
+    # result = messSwitcher[jsonParser['action']](jsonParser['account'],jsonParser['password'])     #两个方括号get到字典和json字符串的值，最后一个小括号传入参数和运行函数
+    sendStr = '%d'%(result)
+    soc.send(sendStr)
+    print('sql查询结果 %d' % result)
+
+def loginCheck(account,password):
+    sqlConnect = mysql.connector.connect(user='root',password='Yt00206.',database='garage')
+    cursor = sqlConnect.cursor()    #处理sql事务的客户端
+    #查询账号
+    cursor.execute('SELECT * FROM accounts WHERE account="%s"' % account)
+    #提交事务
+    #sqlConnect.commit()   #报错
+    #获得结果
+    result = cursor.fetchall()
+    print(result)
+    if result == []:    #如果列表为空，说明这个还没被注册过
+        registerNewAccount(sqlConnect,cursor,account,password)
+        print('注册成功')
+        return RESULT_REGISTER
+    else:               #列表不是空，表示已有该账户
+        if password == result[0][2]: #[0]表示唯一的一个数据，[2]表示密码栏目在返回的tuple里面的第三个位置，例如[(1, u'admin', u'123456')]
+            return RESULT_SUCCESS
+        else :
+            return RESULT_ERROR
+
+def registerNewAccount(sqlConnect,sqlCursor,account,password):
+    sqlCursor.execute('INSERT INTO accounts (account,password) values ("%s","%s")' % (account,password) )
+    sqlConnect.commit()
+
+
+messageSwitcher = {
+    ACTION_LOGIN : loginCheck
+}
+resultSwitcher = {
+
+}
+if __name__ == '__main__':
+    Main()
