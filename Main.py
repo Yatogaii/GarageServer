3f0563d8896eb029bcdb61f98e82f3cef9f96412
Yatogami <781023956@qq.com>
2019-05-08 21:44:42 +0800

    add carGet and carSave

--- Main.py
+++ Main.py
@@ -6,11 +6,14 @@ import json
 import mysql.connector
 #用来判断动作的switcher
 ACTION_LOGIN = 100
-ACTION_GET_CAR = -100
+ACTION_GET_CAR = 100
+ACTION_CAR_SAVE = 123
 #用来检查账号查询结果的switcher
 RESULT_ERROR = 11
 RESULT_REGISTER = 12
 RESULT_SUCCESS = 10
+#存放用户的id，在注册用户或者密码正确时赋值
+global ID
 def Main():
     soc = Socket.socket(Socket.AF_INET,Socket.SOCK_STREAM)
     soc.bind(('0.0.0.0',8080))
@@ -32,15 +35,19 @@ def Main():
 def handleMessage(soc,addr):
     print('start handle message')
     while True:
-    	recStr = soc.recv(1024).decode('utf-8')
-    	jsonParser = json.loads(recStr)
-    	#用字典模拟的switch
-    	result = messageSwitcher[jsonParser['action']](jsonParser['account'],jsonParser['password'])    #模拟switch来处理事务
-    	# result = messSwitcher[jsonParser['action']](jsonParser['account'],jsonParser['password'])     #两个方括号get到字典和json字符串的值，最后一个小括号传入参数和运行函数
+        recStr = soc.recv(1024).decode('utf-8')
+        jsonParser = json.loads(recStr)
+        #用字典模拟的switch
+        if jsonParser['action'] == ACTION_LOGIN:        #action是login的话需要输入参数，进行特殊处理
+            result = messageSwitcher[jsonParser['action']](jsonParser['account'],jsonParser['password'])    #模拟switch来处理事务
+        else:                                           #不需要参数，直接运行函数
+            result = messageSwitcher[jsonParser['action']]()
+        # result = messSwitcher[jsonParser['action']](jsonParser['account'],jsonParser['password'])     #两个方括号get到字典和json字符串的值，最后一个小括号传入参数和运行函数
     	sendStr = '%d'%(result)
-    	soc.send(sendStr)
-    	print('返回结果 %d' % result)
+        soc.send(sendStr)
+        print('返回结果 %d' % result)
 
+#一下几个函数是处理对应的Action的
 def loginCheck(account,password):
     sqlConnect = mysql.connector.connect(user='root',password='Yt00206.',database='garage')
     cursor = sqlConnect.cursor()    #处理sql事务的客户端
@@ -54,12 +61,20 @@ def loginCheck(account,password):
     if result == []:    #如果列表为空，说明这个还没被注册过
         registerNewAccount(sqlConnect,cursor,account,password)
         print('注册成功')
+        ID = account
         return RESULT_REGISTER
     else:               #列表不是空，表示已有该账户
         if password == result[0][2]: #[0]表示唯一的一个数据，[2]表示密码栏目在返回的tuple里面的第三个位置，例如[(1, u'admin', u'123456')]
+            ID = account
             return RESULT_SUCCESS
         else :
             return RESULT_ERROR
+
+def carGet():
+    return 1
+
+def carSave():
+    return 1
 
 def registerNewAccount(sqlConnect,sqlCursor,account,password):
     sqlCursor.execute('INSERT INTO accounts (account,password) values ("%s","%s")' % (account,password) )
@@ -67,10 +82,9 @@ def registerNewAccount(sqlConnect,sqlCursor,account,password):
 
 
 messageSwitcher = {
-    ACTION_LOGIN : loginCheck
-}
-resultSwitcher = {
-
+    ACTION_LOGIN : loginCheck,
+    ACTION_CAR_SAVE : carSave,
+    ACTION_GET_CAR : carGet
 }
 if __name__ == '__main__':
     Main()
