43e253b25d2e1d3d840c4c2fa83440148fa13724
Yatogami <781023956@qq.com>
2019-05-11 15:01:56 +0800

    update

--- Main.py
+++ Main.py
@@ -18,8 +18,9 @@ garageSoc = None
 global ID
 def Main():
 	#这一行代码是开启车库Socket线程获取的
-    #threading.Thread(target=getGarageSocket()).start()
-    soc = Socket.socket(Socket.AF_INET,Socket.SOCK_STREAM)
+    threading.Thread(target=getGarageSocket()).start()
+    print('开启了车库Socket接收线程，同时开始监听APP')
+	soc = Socket.socket(Socket.AF_INET,Socket.SOCK_STREAM)
     soc.bind(('0.0.0.0',8080))
     soc.listen(20)
     while True:
@@ -29,7 +30,7 @@ def Main():
         t = threading.Thread(target=handleMessage,args=(s,addr))
         print('开启了一个新的线程')
         t.start()
-
+		
 #json解析
 # jsonStr = '{"account":"123213","password":"213"}'
 # parJson = json.loads(jsonStr)
@@ -37,8 +38,8 @@ def Main():
 # print(parJson)
 
 def handleMessage(soc,addr):
-    print('start handle message')
     while True:
+		print('开始一轮处理信息')
         recStr = soc.recv(1024).decode('utf-8')
         jsonParser = json.loads(recStr)
         #用字典模拟的switch
@@ -47,12 +48,13 @@ def handleMessage(soc,addr):
         else:                                           #不需要参数，直接运行函数
             result = messageSwitcher[jsonParser['action']]()
         # result = messSwitcher[jsonParser['action']](jsonParser['account'],jsonParser['password'])     #两个方括号get到字典和json字符串的值，最后一个小括号传入参数和运行函数
-        sendStr = '%d'% (result)
+        sendStr = '%d' % (result)
         soc.send(sendStr)
-        print('返回结果 %d' % result)
+        print('返回结果 %s' % sendStr)
 
 #一下几个函数是处理对应的Action的
 def loginCheck(account,password):
+	print('进入账户检查函数')
     sqlConnect = mysql.connector.connect(user='root',password='Yt00206.',database='garage')
     cursor = sqlConnect.cursor()    #处理sql事务的客户端
     #查询账号
@@ -69,15 +71,18 @@ def loginCheck(account,password):
         return RESULT_REGISTER
     else:               #列表不是空，表示已有该账户
         if password == result[0][2]: #[0]表示唯一的一个数据，[2]表示密码栏目在返回的tuple里面的第三个位置，例如[(1, u'admin', u'123456')]
+			print('密码正确')
             ID = account
             return RESULT_SUCCESS
         else :
             return RESULT_ERROR
 
 def carGet():
+	print('有用户取车')
     return 1
 
 def carSave():
+	print('有用户存车')
     return 1
 
 def registerNewAccount(sqlConnect,sqlCursor,account,password):
