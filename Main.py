75b8c6783b195d779199d5e418d37bbfed85ee2c
Yatogami <781023956@qq.com>
2019-05-12 14:29:05 +0800

    update

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ garageSoc = None
 global ID
 def Main():
     #这一行代码是开启车库Socket线程获取的
-    threading.Thread(target=getGarageSocket()).start()
+    #threading.Thread(target=getGarageSocket()).start()
     print('开启了车库Socket接收线程，同时开始监听APP')
     soc = Socket.socket(Socket.AF_INET,Socket.SOCK_STREAM)
     soc.bind(('0.0.0.0',8080))
